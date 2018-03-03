72e170f1abed279b9202350b2e10612c0899bb0a
SungyunKim <ksy056605@gmail.com>
2018-03-02 18:46:20 -0800

    asdf

--- p2/presentation-template/init.js
+++ p2/presentation-template/init.js
@@ -1,31 +1,8 @@
-$(document).ready(function(){
 
-jQuery('.slide:eq(0)').addClass('current');
-
-jQuery(document).keydown(function(e){
-    switch(e.which) {
-        case 38: // up
-            var $current = jQuery('.slide.current'),
-                $prev_embed = $current.prev();
-            jQuery('html, body').animate({scrollTop:$prev_embed.offset().top}, 200);
-            $current.removeClass('current');
-            $prev_embed.addClass('current');
-            return;
-        break;
-
-
-        case 40: // down
-            var $current = jQuery('.slide.current'),
-                $next_embed = $current.next();
-            jQuery('html, body').animate({scrollTop:$next_embed.offset().top}, 200);
-            $current.removeClass('current');
-            $next_embed.addClass('current');
-            return;
-        break;
-
-        default: return; // exit this handler for other keys
-    }
-    e.preventDefault();
-});
-
+$( ".img_r12" ).click(function() {
+	// $(".img_r12").append('<img class = "img_r12" src="img/r12.jpg" width="90%" height="auto">');
+	$(".img_r12").css({
+		"transform" : "rotate("20deg")"
+	});
+  
 });
