372061f9ae4c9abf85771f4df574c76d28979218
SungyunKim <ksy056605@gmail.com>
2018-03-06 17:31:21 -0800

    3/6date

--- p2/presentation-template/init.js
+++ p2/presentation-template/init.js
@@ -1,8 +1,20 @@
 
-$( ".img_r12" ).click(function() {
-	// $(".img_r12").append('<img class = "img_r12" src="img/r12.jpg" width="90%" height="auto">');
-	$(".img_r12").css({
-		"transform" : "rotate("20deg")"
-	});
+// $( ".img_r12" ).click(function() {
+// 	// $(".img_r12").append('<img class = "img_r12" src="img/r12.jpg" width="90%" height="auto">');
+// 	$(".img_r12").css({
+// 		"transform" : "rotate("20deg")"
+// 	});
   
-});
+// });
+
+
+
+$('document').ready(function() {
+	
+	$('#content').clone().insertAfter('#content');
+
+
+	$(document).scroll(function(){
+		if((document.documentElement.clientHeight + $(document).scrollTop()) >= (document.body.offsetHeight/2)+document.documentElement.clientHeight )$(document).scrollTop(0);
+	});
+}); 
