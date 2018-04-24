5f0d451408ccdb85fc2375c27430c60dec495aa0
SungyunKim <ksy056605@gmail.com>
2018-04-24 14:46:37 -0700

    asdf

--- p2/Fstep4/js/index.js
+++ p2/Fstep4/js/index.js
@@ -11,3 +11,24 @@ $('.searchlight')
         });
     })
 ;
+
+$(document).ready(function(){
+    
+    setInterval(function(){
+    
+        $('img').each(function(){
+            var random = Math.floor(Math.random()*3);   
+            $(this).attr('src', 'assets/boop'+random+'.gif')
+        });
+    
+    },1000);
+    
+
+});
+
+var images=['img/hghg1.png','image_url_2','image_url_3','image_url_4'];
+
+setInterval(function(){
+  var url=images[Math.floor(Math.random() * images.length)];
+  document.body.style.backgroundImage = 'url('+url+')';  
+},5000);
