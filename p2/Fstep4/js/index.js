9b7c59edeadf8d8ebe2631bf12a90bcdeebf3ef2
SungyunKim <ksy056605@gmail.com>
2018-04-27 20:40:11 -0700

    asdf

--- p2/Fstep4/js/index.js
+++ p2/Fstep4/js/index.js
@@ -1,4 +1,40 @@
-$('.searchlight')
+
+
+$(document).ready(function(){
+
+
+
+    n = 1;
+    var items = Array('●');
+
+    $('body').on( "mousemove", function(e) {
+        // move #position on mouse move
+        $('#position').css({ 'top': e.pageY+'px', 'left': e.pageX+'px', 'color': "black",});
+        // copy element on mousemove
+        var clone = $('#position').clone();
+        $('.page-'+n).append(clone);
+
+
+        
+        var item = items[Math.floor(Math.random()*items.length)];
+        $('#position').html(item);
+        $('body').append('<div class="page-0 page-'+n+'"></div>');
+    });
+
+
+    $('body').click(function(){
+        $('.page-0').css({ 'opacity': 1,});
+    });
+
+
+    var getRandomColor = function(){
+        var color = ["red","Yellow","blue","#467bd7"]
+        var randomColor = Math.floor(Math.random()*4)
+        return color[randomColor]
+    };
+
+
+    $('.searchlight')
     .on('mousemove', function(event) {
         $(this).addClass('on').css({'margin-left': event.pageX - 150, 'margin-top': event.pageY - 150});
     })
@@ -9,19 +45,28 @@ $('.searchlight')
         $(this).fadeOut(function() {
             $(this).remove();
         });
-    })
-;
-
-$(document).ready(function(){
-    
-    setInterval(function(){
-    
-        $('img').each(function(){
-            var random = Math.floor(Math.random()*3);   
-            $(this).attr('src', 'assets/boop'+random+'.gif')
-        });
-    
-    },1000);
-    
+    });
 
 });
+
+
+
+
+
+
+
+
+
+// $(document).ready(function(){
+    
+//     setInterval(function(){
+    
+//         $('img').each(function(){
+//             var random = Math.floor(Math.random()*3);   
+//             $(this).attr('src', 'assets/boop'+random+'.gif')
+//         });
+    
+//     },1000);
+    
+
+// });
