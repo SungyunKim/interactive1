5f7d5490e5b724eae6f6003ae70808a17c61dce0
SungyunKim <ksy056605@gmail.com>
2018-05-16 22:13:01 -0700

    123

--- p2/Fstep4/Archive/README.txt
+++
@@ -1,3 +0,0 @@
-A Pen created at CodePen.io. You can find this one at https://codepen.io/kensnyder/pen/lfGiI.
-
- This is just a copy of the work done by Jasny: http://jsfiddle.net/jasny/2vj0ec8c/
---
+++ p4/Fstep4/js/index.js
@@ -0,0 +1,74 @@
+
+
+$(document).ready(function(){
+
+
+
+    n = 1;
+    var items = Array('●'); 
+
+    $('body').on( "mousemove", function(e) {
+        // move #position on mouse move
+        $('#position').css({ 'top': e.pageY+'px', 'left': e.pageX+'px', 'color': getRandomColor,});
+        // copy element on mousemove
+        var clone = $('#position').clone();
+        $('.page-'+n).append(clone);
+
+
+        
+        var item = items[Math.floor(Math.random()*items.length)];
+        $('#position').html(item);
+        $('body').append('<div class="page-0 page-'+n+'"></div>');
+    });
+
+
+    $('body').click(function(){
+        // $('.page-0').css({ 'opacity': 0,});
+        $('.after-text').fadeOut();
+        // $('.bgbg').addClass("bgbgmv");
+    });
+
+
+    var getRandomColor = function(){
+        var color = ["#e70000","#3ea521","#f0ff00"]
+        var randomColor = Math.floor(Math.random()*3)
+        return color[randomColor]
+    };
+
+
+    $('.searchlight')
+    .on('mousemove', function(event) {
+        $(this).addClass('on').css({'margin-left': event.pageX - 150, 'margin-top': event.pageY - 150});
+    })
+    .on('mouseout', function(event) {
+        $(this).removeClass('on');
+    })
+    .on('click', function() {
+        $(this).fadeOut(function() {
+            $(this).remove();
+        });
+    });
+
+});
+
+
+
+
+
+
+
+
+
+// $(document).ready(function(){
+    
+//     setInterval(function(){
+    
+//         $('img').each(function(){
+//             var random = Math.floor(Math.random()*3);   
+//             $(this).attr('src', 'assets/boop'+random+'.gif')
+//         });
+    
+//     },1000);
+    
+
+// });
